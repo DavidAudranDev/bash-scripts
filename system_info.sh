6dcdfe1cce513c13a5be08a9cd0fbf0c57761bf1
David Audran <davidhaudran@gmail.com>
2020-04-12 17:40:44 +0200

    Small updates

--- system_info.sh
+++ system_info.sh
@@ -4,11 +4,13 @@ HOST=$(hostname)
 
 CURRENTUSER=$(whoami)
 
-CURRENTDATE=$(date +%D)
+CURRENTDATE=$(date '+%A %B %d of %Y')
+
+CURRENTTIME=$(date '+%X %Z')
 
 IPADDRESS=$(hostname -I | cut -d ' ' -f1)
 
-echo "Today is $CURRENTDATE"
+echo "Today is $CURRENTDATE and time is $CURRENTTIME"
 echo "Hostname: $HOST ($IPADDRESS)"
 echo "User info for $CURRENTUSER:"
 grep $CURRENTUSER /etc/passwd
--- system_maintenance.sh
+++ system_maintenance.sh
@@ -10,13 +10,13 @@ apt-get update
 echo -e "\t\e[1;32m Executing: apt-get upgrade -y\e[0m"
 apt-get upgrade -y
 
+echo -e "\t\e[1;32m Executing: apt-get dist-upgrade\e[0m"
+apt-get dist-upgrade
+
 echo -e "\t\e[1;32m Executing: apt-get -y autoremove\e[0m"
 apt-get -y autoremove
 
 echo -e "\t\e[1;32m Executing: apt-get autoclean\e[0m"
 apt-get autoclean
-
-echo -e "\t\e[1;32m Executing: apt-get dist-upgrade\e[0m"
-apt-get dist-upgrade
 
 echo -e "\e[1;46mScript Terminated\e[0m"
